fb4c0de9c292e2c5cc4ee010b79529caca4a896a
iryawan91 <iryawan1991@gmail.com>
2022-06-22 23:35:47 +0700

    POPUP BOX pada JAVASCRIPT

--- script.js
+++ script.js
@@ -1,4 +1,28 @@
+/*
 var x =10;
-
 console.log('Hello Bandung');
 console.log('Isi dari variabel x :' + x);
+*/
+
+/*
+var nama = prompt('Masukan Nama:');
+alert(nama);
+confirm('Apa anda yakin?');
+
+var tes = confirm('Apa Anda Yakin?');
+if (tes == true){
+    alert('User Menekan OK!');
+}else{
+    alert('User Menekan CANCEL!');
+}
+*/
+
+alert('Selamat Datang');
+var lagi = true;
+
+while (lagi == true){
+    var nama = prompt('Masukan Nama');
+    alert('Halo' + nama);
+
+    lagi = confirm('Coba Lagi?');
+}

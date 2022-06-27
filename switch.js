be9762bf5c5b425fa3ffdeb502c819a7fe3809be
iryawan91 <iryawan1991@gmail.com>
2022-06-27 22:38:04 +0700

    PENGKONDISIAN : SWITCH pada JAVASCRIPT

---
+++ switch.js
@@ -0,0 +1,58 @@
+/*
+var angka=3;
+var angka=prompt('Masukang Angka:');
+
+if (angka==1){
+    alert('Anda memasukan angka 1');
+}else if (angka==2){
+    alert('Anda memasukan angka 2');
+}else if (angka==3){
+    alert('Anda memasukan angka 3');
+}else {
+    alert('Anda memasukan angka yang salah');
+}
+*/
+
+/*
+var angka=prompt('Masukan Angka :');
+
+switch(angka){
+    case '1':
+        alert('Anda memasukan angka 1');
+        break;
+    case '2':
+        alert('Anda memasukan angka 2');
+        break;
+    case '3':
+        alert('Anda memasukan angka 3');
+        break;
+    default:
+        alert('Angka yang dimasukan salah!');
+        break;
+}
+*/
+
+var item=prompt('Masukan Nama Makanan/Minuman : \n (contoh: nasi, daging, susu, hamburger, softdrin)');
+
+switch(item){
+    case 'nasi':
+        alert('Makanan / Minuman SEHAT');
+        break;
+    case 'daging':
+        alert('Makanan / Minuman SEHAT');
+        break;
+    case 'susu':
+        alert('Makanan / Minuman SEHAT');
+        break;
+    case 'hamburger':
+        alert('Makanan / Minuman TIDAK SEHAT');
+        break;
+    case 'softdrink':
+        alert('Makanan / Minuman SEHAT');
+        break;
+    default:
+        alert('Pilihan Salah');
+        break;
+}
+
+
